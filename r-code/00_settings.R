4ad6a31675b7809d85ebd3762a1304313eb2f064
berndpana <Bernd@Bernds-MacBook-Pro.local>
2015-08-07 09:24:17 +0200

    small change

--- r-code/00_settings.R
+++ r-code/00_settings.R
@@ -1,7 +1,7 @@
-library(RPostgreSQL)
+#library(RPostgreSQL)
 
-drv<-dbDriver("PostgreSQL")
-con<-dbConnect(drv,dbname="cacopsylla",host="localhost",port=5432,user="pillo",password="oenothera")
+#drv<-dbDriver("PostgreSQL")
+#con<-dbConnect(drv,dbname="cacopsylla",host="localhost",port=5432,user="pillo",password="")
 #summary(con)
 #dbListTables(con)
 
