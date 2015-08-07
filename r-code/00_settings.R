d21a93dd01e46da7e5ca6946307861a7d6b81b0f
berndpana <Bernd@Bernds-MacBook-Pro.local>
2015-08-07 09:24:17 +0200

    small change

---
+++ r-code/00_settings.R
@@ -0,0 +1,11 @@
+#library(RPostgreSQL)
+
+#drv<-dbDriver("PostgreSQL")
+#con<-dbConnect(drv,dbname="cacopsylla",host="localhost",port=5432,user="pillo",password="")
+#summary(con)
+#dbListTables(con)
+
+#dbDisconnect(con)
+#dbUnloadDriver(drv)
+
+# ... <-dbGetQuery(con,"select * FROM .... ORDER BY id")
